43d130d96d6a6c0f600c743511a1dffdc2b1f243
Eduardo <eduardo.arrospide@utec.edu.pe>
2022-07-09 19:03:58 -0500

    promedios

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import numpy
 import face_recognition
+import random
+import os
 
 #Open and load the json file
 file = open("encoded_faces.json", "r")
@@ -21,17 +23,28 @@ for path, matrix_vector_faces in dataset:
     for dis in face_recognition.face_distance(matrix_vector_faces, faces_encoding[0]):
         answer.append((path, dis))
 
-query_answer = sorted(answer, key = lambda x: x[1])
+query_answer = sorted(answer, key = lambda x: x[1], reverse=True)
 print(query_answer[:5])
+dists = []
+for path, dis in query_answer:
+    dists.append(dis)
+print(numpy.mean(dists))
 
 
-'''
-    N = 5000
-    vector_dist = []
-    for i in range(N):
-        obj_1 = random(data_row)
-        obj_2 = random(data_row)
-        dist = face_recognition.face_distance([obj_1], obj_2[0])
+data_row = dict()
+for file in os.listdir("./lfw/"):
+    subdir = os.path.join("./lfw/", file)
+    for pic in os.listdir(subdir):
+        path = os.path.join(subdir, pic)
+        data_row[pic] = face_recognition.load_image_file(path)
+        
+N = 5000
+vector_dist = []
+for i in range(N):
+    obj_1 = random.choice(list(data_row.values()))
+    obj_2 = random.choice(list(data_row.values()))
+    dist = face_recognition.face_distance(obj_1, obj_2[0])
+    for dis in dist:
+        vector_dist.append(dis)
 
-        vector_dist.append(dist)
-'''
+print(numpy.mean(vector_dist))
