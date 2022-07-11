77d5bb1ebe61d87df505770ed5bcbfe6cd35096e
PedroDominguezUTEC <pedro.dominguez@utec.edu.pe>
2022-07-10 20:19:38 -0500

    - small fixes

--- knn/faiss_index.py
+++ knn/faiss_index.py
@@ -1,20 +1,32 @@
 import faiss
 import numpy as np
+
+
 def knn_faiss(faces_encoding, k , dataset):
-    d = 128
-    M = 32
-    index = faiss.IndexHNSWFlat(d, M)
+
+    index = faiss.IndexHNSWFlat(128, 32)
 
     data = []
-    for path, matrix_vector_faces in dataset:
-        data.append(matrix_vector_faces[0])
-    data = np.float32(np.array(data))
+
+    dataset_index = 0
+    faces_index = 0
+    face_index_to_dataset_index = dict()
     
+    for path, matrix_vector_faces in dataset:
+
+        for face in matrix_vector_faces:
+            face_index_to_dataset_index[faces_index] = dataset_index
+            data.append(face)
+            faces_index+=1
+
+        dataset_index += 1
+
+    data = np.float32(np.array(data))
+
     index.add(data)
 
-    xq = np.float32(np.array([faces_encoding[0]]))
+    xq = np.float32(np.array([xq]))
     
     D, I = index.search(xq, k)
-
-    for idx in I[0]:
-        print(dataset[idx][0])
+    
+    return [dataset[face_index_to_dataset_index[idx]][0] for idx in I[0]]
